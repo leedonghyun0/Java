316bf0a8ee90c3ab7667a5f93730a15deb7bc67c
user <user@DESKTOP-2PGRMF4>
2023-03-21 16:22:00 +0900

    배열 예제

---
+++ arrayEx/src/arrayEx/ArrayEx14.java
@@ -0,0 +1,17 @@
+package arrayEx;
+
+public class ArrayEx14 {
+
+	public static void main(String[] args) {
+		String[] names = {"홍길동", "뉴진스", "아이유", "코요테", "빅뱅"};
+		
+		// (타입 변수명 : 배열 이름) { }
+		
+		for(String name : names) {
+			name += names;
+			System.out.println("이름 : " + name);
+		}
+
+	}
+
+}
